7546d5806f9cbb4720ee0c8088c6f4089abf6d60
David A. Mancilla <david.a.m@live.com>
2021-04-14 02:16:39 -0300

    Update

--- 09-heroes-app/src/app/pages/heroe/heroe.component.ts
+++ 09-heroes-app/src/app/pages/heroe/heroe.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from 'src/app/models/heroe.model';
 
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  styleUrls: ['./heroe.component.css'],
 })
 export class HeroeComponent implements OnInit {
+  heroe: HeroeModel = new HeroeModel();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  guardar(form:NgForm){
+
+    if(form.invalid){
+      console.log("Formulario inválido.")
+      return;
+    }
+
+    console.log(form);
+    console.log(this.heroe);
   }
-
 }
